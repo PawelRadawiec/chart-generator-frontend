f6e02dc075dd9038e3ef621273882d13c861b455
PawelRadawiec <pawel.radaw@gmail.com>
2020-07-30 19:07:20 +0200

    grid layout / template demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MainComponent } from './main/main.component';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavBarComponent,
+    MainComponent,
+    AsideComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
