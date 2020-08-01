e8e410a0bb3a5a79ca90416300aba93fe9fa3eb0
PawelRadawiec <pawel.radaw@gmail.com>
2020-08-01 17:23:02 +0200

    chart js, ng2-charts / install

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataSets } from 'chart.js';
+import { Label, Color } from 'ng2-charts';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  lineChartData: ChartDataSets[] = [
+    {
+      data: [13, 22, 41, 231, 232, 75],
+      label: 'Name 1'
+    },
+    {
+      data: [85, 72, 78, 75, 77, 75],
+      label: 'name 2'
+    },
+  ];
+
+  lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June'];
+
+  lineChartOptions = {
+    responsive: true,
+  };
+
+  lineChartColors: Color[] = [
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(255,255,0,0.28)',
+    },
+  ];
+
+  lineChartLegend = true;
+  lineChartPlugins = [];
+  lineChartType = 'line';
+
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
